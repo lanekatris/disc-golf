9891c96f25fe2f06674ab75a5e9f5654e0fef704
Lane Katris <lane.katris@community.co>
2021-04-14 14:31:05 -0400

    google geo code data has found information on all the courses!

--- src/load-locations-from-courses.ts
+++ src/load-locations-from-courses.ts
@@ -1,34 +1,57 @@
 import { Client } from '@googlemaps/google-maps-services-js';
 import { IsNull } from 'typeorm';
+import a from 'async';
 import { AppConfiguration } from './configuration';
 import { createDbConnection } from './db';
 import { Course } from './entity/course';
 
 const client = new Client();
+const CONCURRENCY = 45;
 export async function loadLocationsFromCourses() {
   const configuration = new AppConfiguration();
   const connection = await createDbConnection(`${configuration.htmlDirectory}/dg.db`);
   const courseRepo = connection.getRepository(Course);
-  const course = await courseRepo.findOne({
+
+  // const course = await courseRepo.findOne({
+  //   where: {
+  //     rawLocationData: IsNull(),
+  //   },
+  // });
+  const courses = await courseRepo.find({
     where: {
       rawLocationData: IsNull(),
     },
   });
-  if (!course) throw new Error('no courses found');
 
-  console.log('Your course', course);
+  if (courses.length === 0) throw new Error('No courses found');
 
-  const response = await client.geocode({
-    params: {
-      address: course.serializeLocation(),
-      key: configuration.googleMapsApiKey,
-    },
-  });
+  let counter = 1;
+  const q = a.queue(async (c: Course, callback) => {
+    console.log(`Processing ${counter}/${courses.length}`);
+    const response = await client.geocode({
+      params: {
+        address: c.serializeLocation(),
+        key: configuration.googleMapsApiKey,
+      },
+    });
 
-  course.didFindLocations = response.data.results.length > 0;
-  course.rawLocationData = JSON.stringify(response.data);
+    c.didFindLocations = response.data.results.length > 0;
+    c.rawLocationData = JSON.stringify(response.data);
 
-  await courseRepo.save(course);
+    await courseRepo.save(c);
+    counter++;
+    callback();
+  // }, 45);
+  }, CONCURRENCY);
 
-  console.log('response', JSON.stringify(response.data, null, 2));
+  // q.drain(async () => {
+  //   // We are done
+  // });
+
+  courses.map((c) => q.push(c));
+  await q.drain();
+  // await q.push(courses);
+  console.log('done!');
+
+  // console.log('response', JSON.stringify(response.data, null, 2));
 }
