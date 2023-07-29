ba8643cf7a4a4a4ee04c9b08c65bdd5e225664dc
Lane Katris <lanekatris@gmail.com>
2023-07-28 22:05:35 -0400

    refactoring and better logging, simplified db schema

--- tests/all.test.ts
+++ tests/all.test.ts
@@ -12,34 +12,35 @@ describe('Create Dataset', function describeAll() {
   it('Load all US courses to sql', async () => {
     await loadCoursesIntoDb(new AppConfiguration());
   });
-  it('Creates csv file', async () => {
-    const configuration = new AppConfiguration();
-    const connection = await createDbConnection(configuration.databasePath);
-    await createCsvFromCoursesDb({
-      configuration,
-      courseRepo: connection.getRepository(Course),
-    });
-  });
-  it('Finds GPS coordinates for courses', async () => {
-    await loadLocationsFromCourses();
-  });
-  it('Evaluates geocode result and updates db', async () => {
-    // await evaluateGeocodes();
-  });
-  it('Create database', async () => {
-    await createDbConnection();
-  });
 
-  it('Set html for course with no initial geocode results', async () => {
-    const configuration = new AppConfiguration();
-    const connection = await createDbConnection(configuration.databasePath);
-    const repo = connection.getRepository(Course);
-    const processor = new LocationProcessorForCourses(repo);
-
-    const course = await repo.findOne({ id: 'woodlands' });
-    if (!course) throw new Error('course not found woodlands');
-    // await processor.processNoLocations(course);
-    await processor.process();
-    console.log(processor.state.getResults());
-  });
+  // it('Creates csv file', async () => {
+  //   const configuration = new AppConfiguration();
+  //   const connection = await createDbConnection(configuration.databasePath);
+  //   await createCsvFromCoursesDb({
+  //     configuration,
+  //     courseRepo: connection.getRepository(Course),
+  //   });
+  // });
+  // it('Finds GPS coordinates for courses', async () => {
+  //   await loadLocationsFromCourses();
+  // });
+  // it('Evaluates geocode result and updates db', async () => {
+  //   // await evaluateGeocodes();
+  // });
+  // it('Create database', async () => {
+  //   await createDbConnection();
+  // });
+  //
+  // it('Set html for course with no initial geocode results', async () => {
+  //   const configuration = new AppConfiguration();
+  //   const connection = await createDbConnection(configuration.databasePath);
+  //   const repo = connection.getRepository(Course);
+  //   const processor = new LocationProcessorForCourses(repo);
+  //
+  //   const course = await repo.findOne({ id: 'woodlands' });
+  //   if (!course) throw new Error('course not found woodlands');
+  //   // await processor.processNoLocations(course);
+  //   await processor.process();
+  //   console.log(processor.state.getResults());
+  // });
 });
